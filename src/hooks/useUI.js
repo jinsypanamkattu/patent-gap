9cc8beda4fef948e1ddc137320d96cb3f4373cf7
jinsypanamkattu <jinsypanamkattu@gmail.com>
2026-03-09 12:25:36 +0100

    Included the Page reload and error scripts

---
+++ src/hooks/useUI.js
@@ -0,0 +1,13 @@
+import { useDispatch } from "react-redux";
+import { setPage, setLoading, setError, clearError } from "../store/slices/uiSlice";
+
+export const useUI = () => {
+  const dispatch = useDispatch();
+
+  return {
+    setPage: (page, data) => dispatch(setPage({ page, data })),
+    setLoading: (bool) => dispatch(setLoading(bool)),
+    setError: (msg) => dispatch(setError(msg)),
+    clearError: () => dispatch(clearError()),
+  };
+};
